6f44cbd53ddf862db29b703f6911990c37f2faa3
poojasharma22 <pooja.sharma@appinventiv.com>
2021-07-27 14:40:17 +0530

    first commit

---
+++ app/src/main/java/com/musicdemo/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.musicdemo
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.musicdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+  private var binding:ActivityMainBinding?=null
+    val audioLink="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+    var musicPlaying :Boolean= false
+    var mediaPlayer:MediaPlayer?=null
+  lateinit  var serviceIntent : Intent
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view= binding?.root
+        setContentView(view)
+        binding?.playStopImageView?.setBackgroundResource(R.drawable.ic_baseline_play_circle_outline_24)
+        serviceIntent = Intent(this,MusicPlayerService::class.java)
+      //  val file  = resources.openRawResourceFd(android.R.raw.sound)
+
+        binding?.playStopImageView?.setOnClickListener {
+          if (!musicPlaying){
+              playAudio()
+              binding?.playStopImageView?.setBackgroundResource(R.drawable.ic_baseline_pause_circle_outline_24)
+              musicPlaying=true
+          }
+            else{
+                stopPlayService()
+              binding?.playStopImageView?.setBackgroundResource(R.drawable.ic_baseline_play_circle_outline_24)
+              musicPlaying=false
+          }
+        }
+    }
+
+    private fun stopPlayService() {
+        mediaPlayer?.stop()
+     /* try {
+       stopService(serviceIntent)
+     }catch (e:SecurityException){
+        Toast.makeText(this,"Error"+e.message,Toast.LENGTH_SHORT).show()
+       }*/
+    }
+
+    private fun playAudio() {
+        mediaPlayer= MediaPlayer.create(this, Uri.parse("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"))
+        mediaPlayer?.start()
+         /*try {
+             startService(serviceIntent)
+         }catch (e:SecurityException){
+             Toast.makeText(this,"Error"+e.message,Toast.LENGTH_SHORT).show()
+         }*/
+
+    }
+}
---
+++ app/src/main/java/com/musicdemo/MusicPlayerService.kt
@@ -0,0 +1,107 @@
+package com.musicdemo
+
+import android.R
+import android.app.Service
+import android.content.Intent
+import android.content.res.AssetFileDescriptor
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.IBinder
+import android.widget.Toast
+import java.io.InputStream
+
+
+class MusicPlayerService : Service(), MediaPlayer.OnCompletionListener,
+    MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
+    MediaPlayer.OnSeekCompleteListener, MediaPlayer.OnInfoListener,
+    MediaPlayer.OnBufferingUpdateListener {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    var link :String?= null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        mediaPlayer=MediaPlayer.create(this, Uri.parse("https://vodafoneapp.s3.ap-south-1.amazonaws.com/gurushala/pages/0.6249899713356712Q2%20P6-%20Art%20Integrated%20Learning.mp3"));
+        mediaPlayer.setOnCompletionListener(this)
+        mediaPlayer.setOnPreparedListener(this)
+        mediaPlayer.setOnErrorListener(this)
+        mediaPlayer.setOnSeekCompleteListener(this)
+        mediaPlayer.setOnInfoListener(this)
+        mediaPlayer.setOnBufferingUpdateListener(this)
+    }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        link= intent?.getStringExtra("AudioLink")
+        mediaPlayer.reset()
+        if (!mediaPlayer.isPlaying){
+            try {
+              //  mediaPlayer.setDataSource()
+               //     mediaPlayer.setDataSource("https://vodafoneapp.s3.ap-south-1.amazonaws.com/gurushala/pages/0.6249899713356712Q2%20P6-%20Art%20Integrated%20Learning.mp3")
+                mediaPlayer.prepareAsync()
+                mediaPlayer.start()
+            }catch (e:Exception){
+                Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show()
+
+
+            }
+        }
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mediaPlayer != null){
+            if (mediaPlayer.isPlaying){
+                mediaPlayer.stop()
+            }
+            mediaPlayer.release()
+        }
+    }
+
+    override fun onCompletion(mp: MediaPlayer?) {
+        if (mp?.isPlaying == true){
+            mp.stop()
+
+        }
+        stopSelf()
+    }
+
+    override fun onPrepared(mp: MediaPlayer?) {
+        if (mp?.isPlaying == true){
+            mp.start()
+
+        }
+    }
+
+    override fun onError(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
+        when(what){
+       MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK->{
+           Toast.makeText(this,"MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK",Toast.LENGTH_SHORT).show()
+
+       }
+            MediaPlayer.MEDIA_ERROR_SERVER_DIED->{
+                Toast.makeText(this,"MEDIA_ERROR_SERVER_DIED",Toast.LENGTH_SHORT).show()
+
+            }
+            MediaPlayer.MEDIA_ERROR_UNKNOWN->{
+                Toast.makeText(this,"MEDIA_ERROR_UNKNOWN",Toast.LENGTH_SHORT).show()
+
+            }
+        }
+        return false
+    }
+
+    override fun onSeekComplete(mp: MediaPlayer?) {
+    }
+
+    override fun onInfo(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
+        return false
+    }
+
+    override fun onBufferingUpdate(mp: MediaPlayer?, percent: Int) {
+    }
+}
